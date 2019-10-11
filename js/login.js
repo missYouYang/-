8cf6960e1f8825f812283e8bfc86e6831c401a51
missYouYang <18437929553@163.com>
2019-10-11 17:24:46 +0800

    页面提交

---
+++ js/login.js
@@ -0,0 +1,17 @@
+function user_login () {
+    var userName = $("#userName").val();
+
+    if($("#userName").val() == null || $("#userName").val() == ''){
+        $("#username_error").text("用户名不能为空");
+        return false;
+    }else{
+        $("#username_error").text("");
+    }
+
+    if($("#password").val() == null || $("#password").val() == ''){
+        $("#password_error").text("密码不能为空");
+        return false;
+    }else {
+        $("#password_error").text("");
+    }
+}
---
+++ js/register.js
@@ -0,0 +1,4 @@
+function u_register() {
+
+    alert("111")
+}
